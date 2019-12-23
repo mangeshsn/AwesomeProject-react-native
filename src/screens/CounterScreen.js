9de00cd7e01f6c5d730a0ddba79235dbc01b3eb0
Mangesh Narkhede <braveleo007@gmail.com>
2019-12-23 22:59:01 +0530

    Adding counter screen

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
+
+
+export default class CounterScreen extends React.Component{
+    render() {
+        return (
+            <View>
+                <Text>Counter Screen</Text>
+            </View>
+        );
+
+    }
+
+}
+
+const styles = StyleSheet.create({
+
+});
